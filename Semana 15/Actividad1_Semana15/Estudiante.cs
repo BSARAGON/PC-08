1e2e49f66d1b9bfad620c14410de560e8cb7399c
BSARAGON <BSARAGON@correo.url.edu.gt>
2025-04-25 22:53:41 -0600

    Add files via upload

---
+++ Semana 15/Actividad1_Semana15/Estudiante.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+
+public class Estudiante
+{
+    string Nombre;
+    int Edad;
+    string Carrera;
+    string Carnet;
+    int NotaAdmision; //propiedad clase Pascal Case 
+
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese su nombre");
+        string nombreIngresado = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad");
+        int edadIngresada = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese su carrera");
+        string carreraIngresada = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carnet");
+        string carnetIngresado = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su nota de admisión");
+        int NotaAdmision = Convert.ToInt32(Console.ReadLine());
+
+        Estudiante objetoEstudiante = new Estudiante(nombreIngresado, edadIngresada, carreraIngresada, carnetIngresado, NotaAdmision);
+        objetoEstudiante.show();
+
+        if (objetoEstudiante.puedeMatricularse()){
+            Console.WriteLine("El estudiante puede matricularse"); 
+        } else {
+            Console.WriteLine("El estudiante no puede matricularse"); 
+        }
+        
+    }
+
+    Estudiante(string Nombre, int Edad, string Carrera, string Carnet, int NotaAdmision){
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Carrera = Carrera;
+        this.Carnet = Carnet;
+        this.NotaAdmision = NotaAdmision;
+    }
+
+    void show(){
+        Console.WriteLine("Datos del estudiante");
+        Console.Write("Nombre: ");
+        Console.WriteLine(this.Nombre);
+        Console.Write("Edad: ");
+        Console.WriteLine(this.Edad);
+        Console.Write("Carrera: ");
+        Console.WriteLine(this.Carrera);
+        Console.Write("Carnet: ");
+        Console.WriteLine(this.Carnet);
+        Console.Write("Nota de Admisión: ");
+        Console.WriteLine(this.NotaAdmision);
+    }
+
+    public bool puedeMatricularse()
+    {
+        return NotaAdmision >= 75 && Carnet.EndsWith("2025");
+    }
+
+}
